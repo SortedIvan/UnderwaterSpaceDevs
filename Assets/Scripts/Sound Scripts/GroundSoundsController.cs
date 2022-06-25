3928591f215704c54fe9e5138b89eeecbf6d059b
Ovcharov,Ivan <450754@student.fontys.nl>
2022-06-26 00:58:34 +0200

    update: added camera movement script, working on making it smooth

--- Assets/Scripts/Sound Scripts/GroundSoundsController.cs
+++ Assets/Scripts/Sound Scripts/GroundSoundsController.cs
@@ -17,7 +17,6 @@ public class GroundSoundsController : MonoBehaviour
     {
         if (play)
         {
-            Debug.Log("FOR REAL MAN, SOUNDS SHOULD BE PLAYING RIGHT NOW");
             source.PlayOneShot(metalSound);
         }
         else source.Stop();
