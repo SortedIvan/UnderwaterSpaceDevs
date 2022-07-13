3828ab4f540d39ea10bba518160c181973724df1
Ovcharov,Ivan <450754@student.fontys.nl>
2022-07-14 01:30:30 +0300

    update: Fish choosing random waypoints + running away when player enters range

--- Assets/Scripts/Creature Scripts/MoveCreature.cs
+++ Assets/Scripts/Creature Scripts/MoveCreature.cs
@@ -6,13 +6,14 @@ public class MoveCreature : MonoBehaviour
 {
     [SerializeField] Transform[] Positions;
     [SerializeField] float objectSpeed;
+    [SerializeField] List<string> objectsThatScareFish;
 
     int nextPosIndex;
     Transform nextPos;
     // Start is called before the first frame update
     void Start()
     {
-        nextPos = Positions[0];
+        nextPos = GetRandomPosition();
     }
 
     // Update is called once per frame
@@ -21,24 +22,49 @@ public class MoveCreature : MonoBehaviour
         MoveGameObject();
     }
 
-    
-
+    private Transform GetRandomPosition()
+    {
+        return Positions[Random.Range(0, Positions.Length)];
+    }
     void MoveGameObject()
     {
         if (transform.position == nextPos.position)
         {
-            nextPosIndex++;
-
-            if (nextPosIndex >= Positions.Length)
-            {
-                nextPosIndex = 0;
-            }
-            nextPos = Positions[nextPosIndex];
+            nextPos = GetRandomPosition();
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, nextPos.position, objectSpeed * Time.deltaTime);
             transform.rotation = Quaternion.LookRotation(nextPos.position - transform.position, transform.up);
+        }
+    }
+
+
+    private Transform FindFurthestObject()
+    {
+        float FurthestDistance = 0;
+        Transform furthestWaypoint = null;
+        foreach (Transform waypoint in Positions)
+        {
+            float ObjectDistance = Vector3.Distance(transform.position, waypoint.position);
+            if (ObjectDistance > FurthestDistance)
+            {
+                furthestWaypoint = waypoint;
+                FurthestDistance = ObjectDistance;
+            }
+        }
+        return furthestWaypoint;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < objectsThatScareFish.Count; i++)
+        {
+            if (objectsThatScareFish[i] == other.tag)
+            {
+                //transform.position = nextPos.position;
+                nextPos = FindFurthestObject();
+            }
         }
     }
 }
